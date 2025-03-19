d5b5ef27a8947d74ac124cf4a32c189ff7a92322
sungje365 <dtw2813@gmail.com>
2025-03-20 06:51:36 +0900

    feat: Recycler 환경 구성

---
+++ app/src/main/java/devcon/map/ui/BaseViewHolder.kt
@@ -0,0 +1,8 @@
+package devcon.map.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewHolder<T>(binding: ViewBinding) : RecyclerView.ViewHolder(binding.root) {
+    abstract fun onBind(item: T)
+}
