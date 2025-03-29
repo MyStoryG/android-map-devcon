fb28f67611d34f9b623870f804eec2b1ce1a33b3
sungje365 <dtw2813@gmail.com>
2025-03-30 04:27:30 +0900

    feat: Remote, Local 데이터 소스 추가

---
+++ app/src/main/java/devcon/map/data/source/remote/SearchRemoteDataSource.kt
@@ -0,0 +1,21 @@
+package devcon.map.data.source.remote
+
+import devcon.map.model.KakaoMapSearchResponse
+import devcon.map.network.KakaoMapApi
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+
+class SearchRemoteDataSource(
+    private val kakaoMapApi: KakaoMapApi,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) {
+    suspend fun getSearchKeyword(
+        page: Int = 1,
+        size: Int = 15,
+        query: String,
+    ): Response<KakaoMapSearchResponse> = withContext(ioDispatcher) {
+        kakaoMapApi.getSearchKeyword(page, size, query)
+    }
+}
