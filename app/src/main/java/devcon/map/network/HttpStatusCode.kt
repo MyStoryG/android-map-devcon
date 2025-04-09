d5cb851de5f45b4ca4b4af8fe02ce42350b13daa
sungje365 <dtw2813@gmail.com>
2025-04-10 03:52:11 +0900

    feat: HTTP 상태 코드 object 추가

---
+++ app/src/main/java/devcon/map/network/HttpStatusCode.kt
@@ -0,0 +1,11 @@
+package devcon.map.network
+
+object HttpStatusCode {
+    const val BAD_REQUEST = 400
+    const val UNAUTHORIZED = 401
+    const val FORBIDDEN = 403
+    const val TOO_MANY_REQUEST = 429
+    const val INTERNAL_SERVER_ERROR = 500
+    const val BAD_GATEWAY = 502
+    const val SERVICE_UNAVAILABLE = 503
+}
